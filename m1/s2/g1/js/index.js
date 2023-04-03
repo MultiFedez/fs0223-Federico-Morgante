0ea70e2bd899fd5ef97c2d15f48e3995a58e2e21
MultiFedez <federicomorgante88@gmail.com>
2023-04-03 16:10:58 +0200

    Esercitazione js

---
+++ m1/s2/g1/js/index.js
@@ -0,0 +1,11 @@
+var anni = prompt ('Quanti anni hai?')
+
+if(anni >= 18 && anni <= 100)
+{document.write ("sei maggiorenne")
+location.href = 'https://www.diretta.it/'
+}else if (anni > 100)
+{document.write ('sei attempato')
+location.href = 'https://it.wikipedia.org/wiki/Sildenafil'
+}else
+{document.write ('sei minorenne')
+location.href = 'https://www.youtube.com/watch?v=6aAnoeK-BsA'}
---
+++ m1/s2/g1/js/name.js
@@ -0,0 +1,7 @@
+/* intestazione esercizio */
+
+var nome = "Federico"
+console.log(nome)
+
+var cognome = 'Morgante'
+console.log(cognome)
