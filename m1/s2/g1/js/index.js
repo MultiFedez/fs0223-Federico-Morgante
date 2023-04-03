aaf3ef7ecf6b1e5ab0574ab84a31cbef8daf68b9
MultiFedez <federicomorgante88@gmail.com>
2023-04-03 16:31:54 +0200

    fix

--- m1/s2/g1/js/index.js
+++ m1/s2/g1/js/index.js
@@ -1,11 +1,13 @@
+/* interazione pagina */
+
 var anni = prompt ('Quanti anni hai?')
 
 if(anni >= 18 && anni <= 100)
 {document.write ("sei maggiorenne")
-location.href = 'https://www.diretta.it/'
+/*location.href = 'https://www.diretta.it/'*/
 }else if (anni > 100)
 {document.write ('sei attempato')
-location.href = 'https://it.wikipedia.org/wiki/Sildenafil'
+/*location.href = 'https://it.wikipedia.org/wiki/Sildenafil'*/
 }else
-{document.write ('sei minorenne')
-location.href = 'https://www.youtube.com/watch?v=6aAnoeK-BsA'}
+{document.write ('sei minorenne')}
+/*location.href = 'https://www.youtube.com/watch?v=6aAnoeK-BsA'}*/
--- m1/s2/g1/js/name.js
+++ m1/s2/g1/js/name.js
@@ -5,3 +5,7 @@ console.log(nome)
 
 var cognome = 'Morgante'
 console.log(cognome)
+
+/* Esempio con altri codici name */
+
+document.getElementById ('nome').innerHTML = nome
