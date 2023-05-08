3000a586eb69897e7f89ad63ba1d075bef4f1bea
MultiFedez <federicomorgante88@gmail.com>
2023-05-08 16:52:50 +0200

    esercizio 1 completo

--- m2/s3-2/g1/script.js
+++ m2/s3-2/g1/script.js
@@ -1,3 +1,5 @@
+// Esercizio 1
+
 class User {
     constructor(firstName, lastName, age, location) {
         this.firstName = firstName;
@@ -6,5 +8,24 @@ class User {
         this.location = location;
     }
 
-
 }
+
+let user1 = new User("Federico", "Morgante", "34", "Manduria");
+console.log(user1);
+
+let user2 = new User("Massimo", "Dinoi", "43", "Firenze");
+console.log(user2);
+
+compareAge = function () {
+    if (user1.age < user2.age) {
+        return user1.firstName + " è più vecchio di " + user2.firstName
+    }
+    else {
+        return user1.firstName + " è più giovane di " + user2.firstName
+    }
+}
+
+console.log(compareAge());
+
+// Esercizio 2
+
