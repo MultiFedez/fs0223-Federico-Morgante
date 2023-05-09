f31b8ea509b7916ce00af43eca3c3a6bf09cc9c8
MultiFedez <federicomorgante88@gmail.com>
2023-05-09 16:40:14 +0200

    nulla assoluto

--- m2/s3-2/g2/script.js
+++ m2/s3-2/g2/script.js
@@ -1,8 +1,22 @@
 // FORM
-let name = document.querySelector("input");
+let nameField = document.getElementsByTagName("input");
 
 // BUTTON
-const buttonSave = document.querySelector(".save");
-const buttonDelete = document.querySelector(".delete");
+const buttonSave = document.getElementById("save");
+const buttonDelete = document.getElementById("delete");
 
+// NEW ARRAY con i nomi dentro
 let savedName = [];
+
+const lastName = document.getElementById("target");
+
+// BUTTON SAVE
+buttonSave.addEventListener("click", save);
+function save(e) {
+  e.preventDefault();
+  localStorage.setItem("Names", JSON.stringify(savedName));
+  savedName.push(nameField.value);
+  localStorage.setItem("Names", JSON.stringify(savedName));
+  lastName.textContent = savedName[savedName.lenght - 1];
+  document.getElementById("form").reset();
+}
