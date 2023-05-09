0bf71d7cec3148247641cf54d35cc86fcf966a1b
MultiFedez <federicomorgante88@gmail.com>
2023-05-09 15:03:54 +0200

    esercizio g2

--- m2/s3-2/g1/script.js
+++ m2/s3-2/g1/script.js
@@ -1,13 +1,12 @@
 // Esercizio 1
 
 class User {
-    constructor(firstName, lastName, age, location) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.location = location;
-    }
-
+  constructor(firstName, lastName, age, location) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.location = location;
+  }
 }
 
 let user1 = new User("Federico", "Morgante", "34", "Manduria");
@@ -17,15 +16,13 @@ let user2 = new User("Massimo", "Dinoi", "43", "Firenze");
 console.log(user2);
 
 compareAge = function () {
-    if (user1.age < user2.age) {
-        return user1.firstName + " è più vecchio di " + user2.firstName
-    }
-    else {
-        return user1.firstName + " è più giovane di " + user2.firstName
-    }
-}
+  if (user1.age < user2.age) {
+    return user1.firstName + " è più vecchio di " + user2.firstName;
+  } else {
+    return user1.firstName + " è più giovane di " + user2.firstName;
+  }
+};
 
 console.log(compareAge());
 
 // Esercizio 2
-
---
+++ m2/s3-2/g2/script.js
@@ -0,0 +1,8 @@
+// FORM
+let name = document.querySelector("input");
+
+// BUTTON
+const buttonSave = document.querySelector(".save");
+const buttonDelete = document.querySelector(".delete");
+
+let savedName = [];
