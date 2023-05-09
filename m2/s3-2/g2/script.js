d50e7286599dd7b4fc274f1fdff3cec916ddecbf
MultiFedez <federicomorgante88@gmail.com>
2023-05-09 17:42:31 +0200

    fix

--- m2/s3-2/g2/script.js
+++ m2/s3-2/g2/script.js
@@ -1,22 +1,19 @@
 // FORM
-let nameField = document.getElementsByTagName("input");
+let nameField = document.getElementsByTagName("input")[0];
 
 // BUTTON
 const buttonSave = document.getElementById("save");
 const buttonDelete = document.getElementById("delete");
 
-// NEW ARRAY con i nomi dentro
-let savedName = [];
-
+// PARAGRAFO in cui inserire il nome stampato
 const lastName = document.getElementById("target");
 
 // BUTTON SAVE
 buttonSave.addEventListener("click", save);
 function save(e) {
   e.preventDefault();
-  localStorage.setItem("Names", JSON.stringify(savedName));
-  savedName.push(nameField.value);
-  localStorage.setItem("Names", JSON.stringify(savedName));
-  lastName.textContent = savedName[savedName.lenght - 1];
-  document.getElementById("form").reset();
+  localStorage.setItem("Names", nameField.value);
+
+  lastName.textContent = localStorage.getItem("Names");
+  document.getElementById("container").reset();
 }
