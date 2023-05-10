30926d938b35086d779ae5a07b029cadff2cf5f9
MultiFedez <federicomorgante88@gmail.com>
2023-05-10 18:32:25 +0200

    prove di spaziatura degli elementi

--- m2/s3-2/g3/script.js
+++ m2/s3-2/g3/script.js
@@ -21,15 +21,19 @@ const renderBooks = function (books) {
     col.classList.add("col-6", "col-md-4", "col-xl-3");
     col.innerHTML = `<div class="card mb-3" style="width: 18rem";>
         <img src="${book.img}" class="card-img-top" style="height: 350px" alt="...">
-        <div class="card-body" style="height:250px" >
+        <div class="card-body d-flex flex-column" style="height:250px">
+          <div class=>
             <h5 class="card-title">${book.title}</h5>
             <p class="card-text h6">${book.category}</p>
             <p class="card-text h6">${book.price}</p>
+          </div>
+          <div class="align-self-end">
             <a href="#" class="btn btn-primary">Buy Now</a>
             <a href="#" id="deleteButton" class="btn btn-danger">Delete</a>
             <a href="#" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
           </svg></a>
+          </div>
         </div>
     </div`;
 
