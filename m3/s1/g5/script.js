acdfb6c29041f9bdeb8d8d18e57d8768ffe1f6d5
MultiFedez <128818940+MultiFedez@users.noreply.github.com>
2023-05-26 13:10:56 +0200

    costanti create

--- m3/s1/g5/script.js
+++ m3/s1/g5/script.js
@@ -1,16 +1,15 @@
 var Smartphone = /** @class */ (function () {
-    function Smartphone(carica, numeroChiamate, costoMinuto) {
+    function Smartphone(carica, numeroChiamate) {
         this.costoMinuto = 0.20;
         this.carica = carica;
         this.numeroChiamate = numeroChiamate;
-        this.costoMinuto = costoMinuto;
     }
     Smartphone.prototype.ricarica = function (euro) {
         this.carica += euro;
         console.log("La ricarica è stata effettuata");
     };
     Smartphone.prototype.numero404 = function () {
-        return "Il tuo credito è" + this.carica;
+        return "Il tuo credito residuo è di € " + this.carica;
     };
     Smartphone.prototype.getNumeroChiamate = function () {
         return this.numeroChiamate;
@@ -20,7 +19,7 @@ var Smartphone = /** @class */ (function () {
         if (costoChiamata < this.carica) {
             this.carica -= costoChiamata;
             this.numeroChiamate++;
-            console.log("chiamata effettuata");
+            console.log("chiamata di ".concat(min, " minuti effettuata "));
         }
         else if (costoChiamata == this.carica) {
             this.carica -= costoChiamata;
@@ -36,3 +35,34 @@ var Smartphone = /** @class */ (function () {
     };
     return Smartphone;
 }());
+var Samsung = new Smartphone(10, 0);
+Samsung.ricarica(10);
+Samsung.chiamata(20);
+console.log(Samsung.numero404());
+console.log(Samsung.getNumeroChiamate());
+Samsung.azzeraChiamate();
+console.log(Samsung.getNumeroChiamate());
+var Oppo = new Smartphone(0, 0);
+Oppo.ricarica(20);
+Oppo.ricarica(30);
+Oppo.chiamata(30);
+Oppo.chiamata(20);
+Oppo.chiamata(15);
+console.log(Oppo.getNumeroChiamate());
+console.log(Oppo.numero404());
+var Xiaomi = new Smartphone(0, 0);
+Xiaomi.ricarica(10);
+Xiaomi.chiamata(10);
+Xiaomi.chiamata(18);
+Xiaomi.chiamata(35);
+Xiaomi.chiamata(25);
+console.log(Xiaomi.getNumeroChiamate());
+var Htc = new Smartphone(0, 0);
+Htc.chiamata(1);
+Htc.ricarica(10);
+Htc.chiamata(8);
+Htc.chiamata(14);
+console.log(Htc.numero404());
+console.log(Htc.getNumeroChiamate());
+Htc.azzeraChiamate();
+console.log(Htc.getNumeroChiamate());
--- m3/s1/g5/script.ts
+++ m3/s1/g5/script.ts
@@ -15,10 +15,9 @@ class Smartphone implements ISmartphone {
     numeroChiamate: number
     costoMinuto: number = 0.20
 
-    constructor(carica: number, numeroChiamate: number, costoMinuto: number) {
+    constructor(carica: number, numeroChiamate: number,) {
         this.carica = carica
         this.numeroChiamate = numeroChiamate
-        this.costoMinuto = costoMinuto
     }
 
     ricarica(euro: number): void {
@@ -27,7 +26,7 @@ class Smartphone implements ISmartphone {
     }
 
     numero404(): string {
-        return "Il tuo credito è" + this.carica
+        return "Il tuo credito residuo è di € " + this.carica
     }
 
     getNumeroChiamate(): number {
@@ -38,7 +37,7 @@ class Smartphone implements ISmartphone {
         if (costoChiamata < this.carica) {
             this.carica -= costoChiamata
             this.numeroChiamate++
-            console.log("chiamata effettuata");
+            console.log(`chiamata di ${min} minuti effettuata `)
         } else if (costoChiamata == this.carica) {
             this.carica -= costoChiamata
             this.numeroChiamate++
@@ -53,4 +52,48 @@ class Smartphone implements ISmartphone {
     }
 
 }
+
+const Samsung = new Smartphone(10, 0)
+Samsung.ricarica(10)
+Samsung.chiamata(20)
+console.log(Samsung.numero404());
+console.log(Samsung.getNumeroChiamate());
+Samsung.azzeraChiamate()
+console.log(Samsung.getNumeroChiamate());
+
+
+const Oppo = new Smartphone(0, 0)
+Oppo.ricarica(20)
+Oppo.ricarica(30)
+Oppo.chiamata(30)
+Oppo.chiamata(20)
+Oppo.chiamata(15)
+console.log(Oppo.getNumeroChiamate());
+console.log(Oppo.numero404());
+
+
+const Xiaomi = new Smartphone(0, 0)
+Xiaomi.ricarica(10)
+Xiaomi.chiamata(10)
+Xiaomi.chiamata(18)
+Xiaomi.chiamata(35)
+Xiaomi.chiamata(25)
+console.log(Xiaomi.getNumeroChiamate());
+
+
+const Htc = new Smartphone(0, 0)
+Htc.chiamata(1)
+Htc.ricarica(10)
+Htc.chiamata(8)
+Htc.chiamata(14)
+console.log(Htc.numero404());
+console.log(Htc.getNumeroChiamate());
+Htc.azzeraChiamate()
+console.log(Htc.getNumeroChiamate());
+
+
+
+
+
+
 
