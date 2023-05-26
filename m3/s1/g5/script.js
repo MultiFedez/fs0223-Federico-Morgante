96c39e5c9a5de70bb931ac20ee63426eb9859582
MultiFedez <128818940+MultiFedez@users.noreply.github.com>
2023-05-26 16:45:23 +0200

    Esercizio finito

--- m3/s1/g5/script.js
+++ m3/s1/g5/script.js
@@ -1,9 +1,39 @@
+//  CLASSE EXTRA
+var Call = /** @class */ (function () {
+    function Call(id, durata, dataOra) {
+        this.id = id;
+        this.durata = durata;
+        this.dataOra = dataOra;
+    }
+    return Call;
+}());
+// CREAZIONE CLASSE E IMPLEMENTAZIONE
 var Smartphone = /** @class */ (function () {
     function Smartphone(carica, numeroChiamate) {
         this.costoMinuto = 0.20;
+        this.registroChiamate = [];
         this.carica = carica;
         this.numeroChiamate = numeroChiamate;
     }
+    // EXTRA ESERCIZIO
+    Smartphone.prototype.getRegistroChiamate = function () {
+        console.log(this.registroChiamate);
+    };
+    Smartphone.prototype.filtraChiamatePerDataOra = function (d, h) {
+        var filtro = [];
+        this.registroChiamate.forEach(function (c) {
+            if (d == c.dataOra.getDate() && h == c.dataOra.getHours()) {
+                filtro.push(c);
+            }
+        });
+        if (filtro.length == 0) {
+            console.log("non ci sono risultati");
+        }
+        else {
+            console.log(filtro);
+        }
+    };
+    // METODI 
     Smartphone.prototype.ricarica = function (euro) {
         this.carica += euro;
         console.log("La ricarica è stata effettuata");
@@ -19,6 +49,8 @@ var Smartphone = /** @class */ (function () {
         if (costoChiamata < this.carica) {
             this.carica -= costoChiamata;
             this.numeroChiamate++;
+            var now = new Date();
+            this.registroChiamate.push(new Call(this.numeroChiamate, min, now));
             console.log("chiamata di ".concat(min, " minuti effettuata "));
         }
         else if (costoChiamata == this.carica) {
@@ -35,13 +67,19 @@ var Smartphone = /** @class */ (function () {
     };
     return Smartphone;
 }());
+// PRIMA ISTANZA
 var Samsung = new Smartphone(10, 0);
 Samsung.ricarica(10);
 Samsung.chiamata(20);
+Samsung.chiamata(10);
+console.log("filtra");
+Samsung.filtraChiamatePerDataOra(26, 15);
 console.log(Samsung.numero404());
 console.log(Samsung.getNumeroChiamate());
+Samsung.getRegistroChiamate();
 Samsung.azzeraChiamate();
 console.log(Samsung.getNumeroChiamate());
+// SECONDA ISTANZA   
 var Oppo = new Smartphone(0, 0);
 Oppo.ricarica(20);
 Oppo.ricarica(30);
@@ -50,6 +88,7 @@ Oppo.chiamata(20);
 Oppo.chiamata(15);
 console.log(Oppo.getNumeroChiamate());
 console.log(Oppo.numero404());
+// TERZA ISTANZA 
 var Xiaomi = new Smartphone(0, 0);
 Xiaomi.ricarica(10);
 Xiaomi.chiamata(10);
@@ -57,6 +96,7 @@ Xiaomi.chiamata(18);
 Xiaomi.chiamata(35);
 Xiaomi.chiamata(25);
 console.log(Xiaomi.getNumeroChiamate());
+// QUARTA ISTANZA 
 var Htc = new Smartphone(0, 0);
 Htc.chiamata(1);
 Htc.ricarica(10);
@@ -66,3 +106,14 @@ console.log(Htc.numero404());
 console.log(Htc.getNumeroChiamate());
 Htc.azzeraChiamate();
 console.log(Htc.getNumeroChiamate());
+/*Traccia Extra Dell'esercizio:
+
+Aggiungi una proprietà registroChiamate che contenga un array di oggetti contenenti i seguenti dati:
+id
+durata
+Data ed ora della chiamata
+
+Aggiorna la classe in modo che il registro funzioni, e crea i seguenti metodi:
+mostraRegistroChiamate() => mostra tutte le chiamate effettuate
+filtraChiamatePerDataOra() => mostra solo le chiamate effettuate in una determinata data ed ora
+ */ 
--- m3/s1/g5/script.ts
+++ m3/s1/g5/script.ts
@@ -1,25 +1,63 @@
-interface ISmartphone {
+// CREAZIONE INTERFACCIA
+interface LoSmartphone {
     carica: number
     numeroChiamate: number
     costoMinuto: number
+    registroChiamate: Call[]
 
+    getRegistroChiamate(): void
+    filtraChiamatePerDataOra(d: number, h: number): void
     ricarica(euro: number): void
     numero404(): string
     getNumeroChiamate(): number
     chiamata(min: number): void
     azzeraChiamate(): void
 }
+//  CLASSE EXTRA
+class Call {
+    id: number;
+    durata: number;
+    dataOra: Date;
 
-class Smartphone implements ISmartphone {
+    constructor(id: number, durata: number, dataOra: Date) {
+        this.id = id;
+        this.durata = durata;
+        this.dataOra = dataOra;
+    }
+}
+// CREAZIONE CLASSE E IMPLEMENTAZIONE
+class Smartphone implements LoSmartphone {
     carica: number
     numeroChiamate: number
     costoMinuto: number = 0.20
+    registroChiamate: Call[] = []
 
     constructor(carica: number, numeroChiamate: number,) {
         this.carica = carica
         this.numeroChiamate = numeroChiamate
     }
 
+    // EXTRA ESERCIZIO
+    getRegistroChiamate(): void {
+        console.log(this.registroChiamate);
+    }
+
+    filtraChiamatePerDataOra(d: number, h: number): void {
+        let filtro: Call[] = []
+        this.registroChiamate.forEach((c: Call) => {
+            if (d == c.dataOra.getDate() && h == c.dataOra.getHours()) {
+                filtro.push(c)
+            }
+        })
+        if (filtro.length == 0) {
+            console.log("non ci sono risultati");
+
+        } else {
+            console.log(filtro);
+        }
+    }
+
+    // METODI 
     ricarica(euro: number): void {
         this.carica += euro
         console.log("La ricarica è stata effettuata");
@@ -32,11 +70,14 @@ class Smartphone implements ISmartphone {
     getNumeroChiamate(): number {
         return this.numeroChiamate
     }
+
     chiamata(min: number): void {
         let costoChiamata = this.costoMinuto * min
         if (costoChiamata < this.carica) {
             this.carica -= costoChiamata
             this.numeroChiamate++
+            let now: Date = new Date();
+            this.registroChiamate.push(new Call(this.numeroChiamate, min, now))
             console.log(`chiamata di ${min} minuti effettuata `)
         } else if (costoChiamata == this.carica) {
             this.carica -= costoChiamata
@@ -50,18 +91,22 @@ class Smartphone implements ISmartphone {
     azzeraChiamate(): void {
         this.numeroChiamate = 0
     }
-
 }
-
+// PRIMA ISTANZA
 const Samsung = new Smartphone(10, 0)
 Samsung.ricarica(10)
 Samsung.chiamata(20)
+Samsung.chiamata(10)
+console.log("filtra");
+Samsung.filtraChiamatePerDataOra(26, 15)
 console.log(Samsung.numero404());
 console.log(Samsung.getNumeroChiamate());
+Samsung.getRegistroChiamate();
 Samsung.azzeraChiamate()
 console.log(Samsung.getNumeroChiamate());
 
 
+// SECONDA ISTANZA   
 const Oppo = new Smartphone(0, 0)
 Oppo.ricarica(20)
 Oppo.ricarica(30)
@@ -71,7 +116,7 @@ Oppo.chiamata(15)
 console.log(Oppo.getNumeroChiamate());
 console.log(Oppo.numero404());
 
-
+// TERZA ISTANZA 
 const Xiaomi = new Smartphone(0, 0)
 Xiaomi.ricarica(10)
 Xiaomi.chiamata(10)
@@ -80,7 +125,7 @@ Xiaomi.chiamata(35)
 Xiaomi.chiamata(25)
 console.log(Xiaomi.getNumeroChiamate());
 
-
+// QUARTA ISTANZA 
 const Htc = new Smartphone(0, 0)
 Htc.chiamata(1)
 Htc.ricarica(10)
@@ -97,3 +142,14 @@ console.log(Htc.getNumeroChiamate());
 
 
 
+/*Traccia Extra Dell'esercizio:
+
+Aggiungi una proprietà registroChiamate che contenga un array di oggetti contenenti i seguenti dati:
+id
+durata
+Data ed ora della chiamata
+
+Aggiorna la classe in modo che il registro funzioni, e crea i seguenti metodi:
+mostraRegistroChiamate() => mostra tutte le chiamate effettuate
+filtraChiamatePerDataOra() => mostra solo le chiamate effettuate in una determinata data ed ora
+ */
