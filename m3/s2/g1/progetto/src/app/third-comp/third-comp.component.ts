0fa454a80bc093cf5e76b5271a057af76d42d7da
MultiFedez <128818940+MultiFedez@users.noreply.github.com>
2023-05-29 17:00:58 +0200

    prova Angular

---
+++ m3/s2/g1/progetto/src/app/third-comp/third-comp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-third-comp',
+  templateUrl: './third-comp.component.html',
+  styleUrls: ['./third-comp.component.scss']
+})
+export class ThirdCompComponent {
+
+}
